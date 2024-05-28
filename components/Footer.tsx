813e1f9687df655aa4ec45e068db1d5e835fe29b
iamdarwinsai <darushyam143@gmail.com>
2024-05-29 02:26:28 +0530

    init asdas

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,9 +35,9 @@ export function Footer() {
     <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto md:px-8 mt-3 border-t">
       <div className="max-w-lg sm:mx-auto sm:text-center">
         <p className="leading-relaxed mt-2 text-[15px]">
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book.
+          Lorem Ipsum has been the industry`&apos;s standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
         </p>
       </div>
       <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
